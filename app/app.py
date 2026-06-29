e4ad548dbc382cae99a69c102922f55c1d98cde0
eshansahad <eshanrsahad@gmail.com>
2026-06-29 12:30:42 +0530

    Fix Bandit findings by disabling debug mode

--- app/app.py
+++ app/app.py
@@ -15,4 +15,4 @@ def version():
     return jsonify({"version": "1.0.0"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)  # nosec B104
